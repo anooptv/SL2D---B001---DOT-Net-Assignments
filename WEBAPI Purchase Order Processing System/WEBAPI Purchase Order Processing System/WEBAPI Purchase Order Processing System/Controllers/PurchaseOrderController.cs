e3695d0549c7ab2afcd4d228efd7d782cdfb74f9
Administrator <Administrator@DOTNET>
2019-02-26 17:58:38 -0800

    first commit

---
+++ WEBAPI Purchase Order Processing System/WEBAPI Purchase Order Processing System/WEBAPI Purchase Order Processing System/Controllers/PurchaseOrderController.cs
@@ -0,0 +1,105 @@
+﻿using WEBAPI_Purchase_Order_Processing_System.DataObjects;
+using WEBAPI_Purchase_Order_Processing_System.Models;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+
+namespace WEBAPI_Purchase_Order_Processing_System.Controllers
+{
+    [RoutePrefix("api/purchase")]
+    public class PurchaseOrderController : ApiController
+    {
+        private PODbEntities dbContext = new PODbEntities();
+
+        [Route("create")]
+        [HttpPost]
+        public void CreatePurchaseOrder(PurchaseOrderModel POModel)
+        {
+            var dbMaster = new POMASTER
+            {
+                PONO = POModel.PONumber,
+                PODATE = POModel.Date,
+                SUPLNO = POModel.SupplierNumber
+            };
+
+            var dbDetail = new PODETAIL
+            {
+                PONO = POModel.PONumber,
+                ITCODE = POModel.ItemCode,
+                QTY = POModel.Quantity
+            };
+
+            dbContext.POMASTER.Add(dbMaster);
+            dbContext.PODETAIL.Add(dbDetail);
+            dbContext.SaveChanges();
+        }
+
+        [Route("update")]
+        [HttpPost]
+        public void UpdatePurchaseOrder(PurchaseOrderModel POModel)
+        {
+            var dbMaster = new POMASTER
+            {
+                PONO = POModel.PONumber,
+                PODATE = POModel.Date,
+                SUPLNO = POModel.SupplierNumber
+            };
+
+            var dbDetail = new PODETAIL
+            {
+                PONO = POModel.PONumber,
+                ITCODE = POModel.ItemCode,
+                QTY = POModel.Quantity
+            };
+
+            dbContext.Entry(dbMaster).State = EntityState.Modified;
+            dbContext.Entry(dbDetail).State = EntityState.Modified;
+            dbContext.SaveChanges();
+        }
+
+        [Route("delete/{orderNumber}")]
+        public void DeletePurchaseOrder(string orderNumber)
+        {
+            var dbMaster = dbContext.POMASTER.FirstOrDefault(x => x.PONO == orderNumber);
+            var dbDetail = dbContext.PODETAIL.FirstOrDefault(x => x.PONO == orderNumber);
+            if (dbMaster != null)
+            {
+                dbContext.POMASTER.Remove(dbMaster);
+                dbContext.SaveChanges();
+            }
+            if (dbDetail != null)
+            {
+                dbContext.PODETAIL.Remove(dbDetail);
+                dbContext.SaveChanges();
+            }
+        }
+
+        [Route("delete")]
+        public List<PurchaseOrderModel> GetPurchaseOrders()
+        {
+            return dbContext.PODETAIL.Select(x => new PurchaseOrderModel
+            {
+                PONumber = x.PONO,
+                Date = x.POMASTER.PODATE.Value,
+                ItemCode = x.ITCODE,
+                Quantity = x.QTY.Value,
+                SupplierNumber = x.POMASTER.SUPLNO
+            }).ToList();
+        }
+
+        [Route("get/{orderNumber}")]
+        public PurchaseOrderModel GetPurchaseOrder(string code)
+        {
+            var x = dbContext.PODETAIL.Include(y => y.POMASTER).FirstOrDefault(y => y.PONO == code);
+            return x != null ? new PurchaseOrderModel
+            {
+                PONumber = x.PONO,
+                Date = x.POMASTER.PODATE.Value,
+                ItemCode = x.ITCODE,
+                Quantity = x.QTY.Value,
+                SupplierNumber = x.POMASTER.SUPLNO
+            } : null;
+        }
+    }
+}
---
+++ WEBAPI Purchase Order Processing System/WEBAPI Purchase Order Processing System/WEBAPI Purchase Order Processing System/Controllers/SupplierController.cs
@@ -0,0 +1,84 @@
+﻿using WEBAPI_Purchase_Order_Processing_System.DataObjects;
+using WEBAPI_Purchase_Order_Processing_System.Models;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+
+namespace WEBAPI_Purchase_Order_Processing_System.Controllers
+{
+    [RoutePrefix("api/supplier")]
+    public class SupplierController : ApiController
+    {
+        private PODbEntities dbContext = new PODbEntities();
+
+        /// <summary>
+        /// Add new supplier
+        /// </summary>
+        /// <param name="supplier"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("create")]
+        public string CreateSupplier(SupplierModel supplier)
+        {
+            var dbSupplier = new SUPPLIER
+            {
+                SUPLADDR = supplier.Address,
+                SUPLNAME = supplier.Name,
+                SUPLNO = supplier.Id
+            };
+            dbContext.SUPPLIER.Add(dbSupplier);
+            dbContext.SaveChanges();
+            return dbSupplier.SUPLNO;
+        }
+
+        [HttpPost]
+        [Route("update")]
+        public void UpdateSupplier(SupplierModel supplier)
+        {
+            var dbSupplier = new SUPPLIER
+            {
+                SUPLADDR = supplier.Address,
+                SUPLNAME = supplier.Name,
+                SUPLNO = supplier.Id
+            };
+            dbContext.Entry(dbSupplier).State = EntityState.Modified;
+            dbContext.SaveChanges();
+        }
+
+        [Route("delete/{id}")]
+        public void DeleteSupplier(string id)
+        {
+            var supplier = dbContext.SUPPLIER.FirstOrDefault(x => x.SUPLNO == id);
+            if (supplier != null)
+            {
+                dbContext.SUPPLIER.Remove(supplier);
+                dbContext.SaveChanges();
+            }
+        }
+
+        [Route("get")]
+        public List<SupplierModel> GetSuppliers()
+        {
+            return dbContext.SUPPLIER.Select(x => new SupplierModel
+            {
+                Address = x.SUPLADDR,
+                Id = x.SUPLNO,
+                Name = x.SUPLNAME
+            }).ToList();
+        }
+
+        [Route("get/{id}")]
+        public SupplierModel GetSupplier(string id)
+        {
+            var x = dbContext.SUPPLIER.FirstOrDefault(y => y.SUPLNO == id);
+            return x != null ? new SupplierModel
+            {
+                Address = x.SUPLADDR,
+                Id = x.SUPLNO,
+                Name = x.SUPLNAME
+            } : null;
+        }
+
+    }
+}
